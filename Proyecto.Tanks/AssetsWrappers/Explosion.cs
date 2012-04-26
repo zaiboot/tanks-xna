44928e1bb6794a9035ad10fb2231992a2097c20b
luishum17@gmail.com <luishum17@gmail.com@c3af833b-81c6-3350-b1c5-f946ac9a68a7>
2012-04-26 21:36:25 +0000

    Explosion when fire

---
+++ Proyecto.Tanks/AssetsWrappers/Explosion.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Proyecto.Tanks.AssetsWrappers
+{
+    public class Explosion : BaseAsset
+    {
+
+        public struct ParticleData
+        {
+            public float BirthTime;
+            public float MaxAge;
+            public Vector2 OrginalPosition;
+            public Vector2 Accelaration;
+            public Vector2 Direction;
+            public Vector2 Position;
+            public float Scaling;
+            public Color ModColor;
+        }
+
+
+        private Game owner;
+
+        Texture2D explosionTexture;
+        Color[,] explosionColorArray;
+        List<ParticleData> particleList = new List<ParticleData>();
+
+        Random randomizer = new Random();
+
+
+        public Explosion(Game owner)
+        {
+            this.owner = owner;
+        }
+
+        public override void LoadResources(Microsoft.Xna.Framework.Content.ContentManager content)
+        {
+            explosionTexture = content.Load<Texture2D>("explosion");
+
+            explosionColorArray = Utils.TextureTo2DArray(explosionTexture);
+        }
+
+        public override void Update(Microsoft.Xna.Framework.GameTime gameTime)
+        {
+            if (particleList.Count > 0)
+                UpdateParticles(gameTime);
+        }
+
+        public override void Draw(Microsoft.Xna.Framework.Graphics.SpriteBatch spriteBatch)
+        {
+          //spriteBatch.Begin(SpriteSortMode.Deferred, BlendState.Additive);
+          //  DrawExplosion(spriteBatch);
+          //spriteBatch.End();
+        }
+
+        public void DrawExplosion (Microsoft.Xna.Framework.Graphics.SpriteBatch spriteBatch)
+        {
+            spriteBatch.Begin(SpriteSortMode.Deferred, BlendState.Additive);
+            
+            for (int i = 0; i < particleList.Count; i++)
+            {
+                ParticleData particle = particleList[i];
+                spriteBatch.Draw(explosionTexture, particle.Position, null, particle.ModColor, i, new Vector2(256, 256), particle.Scaling, SpriteEffects.None, 1);
+
+            }
+            
+            spriteBatch.End();
+        }
+
+        private void AddExplosionParticle(Vector2 explosionPos, float explosionSize, float maxAge, GameTime gameTime)
+        {
+            ParticleData particle = new ParticleData();
+
+            particle.OrginalPosition = explosionPos;
+            particle.Position = particle.OrginalPosition;
+
+            particle.BirthTime = (float)gameTime.TotalGameTime.TotalMilliseconds;
+            particle.MaxAge = maxAge;
+            particle.Scaling = 0.25f;
+            particle.ModColor = Color.White;
+
+            float particleDistance = (float)randomizer.NextDouble() * explosionSize;
+            Vector2 displacement = new Vector2(particleDistance, 0);
+            float angle = MathHelper.ToRadians(randomizer.Next(360));
+            displacement = Vector2.Transform(displacement, Matrix.CreateRotationZ(angle));
+
+            particle.Direction = displacement * 2.0f;
+            particle.Accelaration = -particle.Direction;
+
+            particleList.Add(particle);
+        }
+
+        private void UpdateParticles(GameTime gameTime)
+        {
+            float now = (float)gameTime.TotalGameTime.TotalMilliseconds;
+            for (int i = particleList.Count - 1; i >= 0; i--)
+            {
+                ParticleData particle = particleList[i];
+                float timeAlive = now - particle.BirthTime;
+
+                if (timeAlive > particle.MaxAge)
+                {
+                    particleList.RemoveAt(i);
+                }
+                else
+                {
+                    float relAge = timeAlive / particle.MaxAge;
+
+                    particle.Position = 0.5f * particle.Accelaration * relAge * relAge + particle.Direction * relAge + particle.OrginalPosition;
+
+                    float invAge = 1.0f - relAge;
+                    particle.ModColor = new Color(new Vector4(invAge, invAge, invAge, invAge));
+
+                    Vector2 positionFromCenter = particle.Position - particle.OrginalPosition;
+                    float distance = positionFromCenter.Length();
+                    particle.Scaling = (50.0f + distance) / 200.0f;
+
+                    particleList[i] = particle;
+                }
+            }
+        }
+
+        public void AddExplosion(Vector2 explosionPos, int numberOfParticles, float size, float maxAge, GameTime gameTime)
+        {
+            for (int i = 0; i < numberOfParticles; i++)
+                AddExplosionParticle(explosionPos, size, maxAge, gameTime);
+
+            float rotation = (float)randomizer.Next(10);
+
+            Matrix mat = Matrix.CreateTranslation(-explosionTexture.Width / 2, -explosionTexture.Height / 2, 0) * Matrix.CreateRotationZ(rotation) * Matrix.CreateScale(size / (float)explosionTexture.Width * 2.0f) * Matrix.CreateTranslation(explosionPos.X, explosionPos.Y, 0);
+        }
+
+    }
+}
