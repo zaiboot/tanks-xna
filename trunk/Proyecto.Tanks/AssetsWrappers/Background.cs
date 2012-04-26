a3e712d7c6c3244add2f5309837f46e476e4dcee
zaiboot@gmail.com <zaiboot@gmail.com@c3af833b-81c6-3350-b1c5-f946ac9a68a7>
2012-04-26 04:06:35 +0000

    Importing version 0.1.

---
+++ trunk/Proyecto.Tanks/AssetsWrappers/Background.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Proyecto.Tanks.AssetsWrappers
+{
+    /// <summary>
+    /// The foreground that will be created dinamically
+    /// </summary>
+    public class Background : BaseAsset
+    {
+        /// <summary>
+        /// The game that we are running
+        /// </summary>
+        private Game owner;
+
+        /// <summary>
+        /// The image that we will repeat over
+        /// </summary>
+        private Texture2D backgroundShape;
+
+        /// <summary>
+        /// Where the background starts
+        /// </summary>
+        private Vector2 position;
+
+        private List<Vector2> nonTransparentPoints = new List<Vector2>();
+
+        public IList<Vector2> NonTransparentPoints { get { return nonTransparentPoints; } }
+
+        public Background(Game owner)
+        {
+            this.owner = owner;
+            position = new Vector2(0, 0);
+        }
+
+
+        public override void LoadResources(Microsoft.Xna.Framework.Content.ContentManager content)
+        {
+
+            backgroundShape = new Texture2D(owner.GraphicsDevice, owner.Window.ClientBounds.Width, owner.Window.ClientBounds.Height);
+            int width = owner.Window.ClientBounds.Width;
+            int height = owner.Window.ClientBounds.Height;
+            Color[] backgroundData = new Color[width * height];
+
+            // Colour the entire texture transparent first.
+            for (int i = 0; i < backgroundData.Length; i++)
+                backgroundData[i] = Color.Transparent;
+
+            Texture2D brick = content.Load<Texture2D>("brick");
+            Color[,] brickContent = Utils.TextureTo2DArray(content.Load<Texture2D>("brick"));
+            Random randomObject = new Random();
+
+            // this will use same smoke radomizer function we use for smoke generating.
+            double rand1 = randomObject.NextDouble() + 1;
+            double rand2 = randomObject.NextDouble() + 2;
+            double rand3 = randomObject.NextDouble() + 3;
+
+            float offset = height / 2;
+            float peakheight = height;
+            float flatness = 70;
+
+            // Ulacit: For each wave, we first draw a random value between 0 and 1. We also offset it a bit, so the first one becomes between the [0,1] range,
+            // the second between the [1,2] range and the last one between the [2,3] range. For our 3 waves, these values will divide the peakheight
+            // and the flatness, so wave 3 will be lower and shorter than waves 1 and 2. Furthermore, you see we’re also adding the random values inside
+            // the Sine method. Otherwise, all 3 waves would start exactly at the Y coordinate specified by ‘offset’, making this point fixed each time
+            // we would restart the game.
+            
+            
+            
+
+
+            for (int x = 0; x < width; x++)
+            {
+                
+                for (int y = 0; y < height; y++)
+                {
+                    //Areas that cannot have brick colors at all, 
+                    //these are the spaces for the 4 players
+                    if ((x > 60 || y > 60)
+                        && (x < width - 60 || y > 60)
+                        && (x < width - 60 || y < height - 60)
+                        && (x > 60 || y < height - 60)
+
+                        )
+                    {
+
+                        double height1 = peakheight / rand1 * Math.Sin((float)x / flatness * rand1 + rand1);
+                        height1 += peakheight / rand2 * Math.Cos((float)y / flatness * rand2 + rand2);
+                        height1 += peakheight / rand3 * Math.Tanh((float)x / flatness * rand3 + rand3);
+                        height1 += offset;
+                        
+                        //Define wheter we add the brick content or it will be transparent
+                        if (height1 > y)
+                        {
+
+                            backgroundData[x + y * width] = brickContent[x % brick.Width, y % brick.Height];
+                            nonTransparentPoints.Add(new Vector2(x, y));
+                        }
+                    }
+
+                }
+            }
+            backgroundShape.SetData(backgroundData);
+
+
+            //Here we need to calculate the convex hull for this one also update it every time
+            //that the background is hit by a bullet, since we it will change all.
+
+        }
+
+        public override void Update(Microsoft.Xna.Framework.GameTime gameTime)
+        {
+
+        }
+
+        public override void Draw(Microsoft.Xna.Framework.Graphics.SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(backgroundShape, position, Color.White);
+        }
+    }
+}
---
+++ trunk/Proyecto.Tanks/AssetsWrappers/Tank.cs
@@ -0,0 +1,335 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using System.Linq;
+using System;
+using System.Collections.Generic;
+
+namespace Proyecto.Tanks.AssetsWrappers
+{
+    internal enum Orientation
+    {
+        DOWN,
+        UP,
+        LEFT,
+        RIGHT
+
+    };
+
+    /// <summary>
+    /// This is a tank.
+    /// What the player controls and shoots bullets, but only one at the time.
+    /// Until the bullet is out of bounds or hit some obstacle, the thank cannot shoot another bullet.
+    /// </summary>
+    public class Tank : BaseAsset
+    {
+        /// <summary>
+        /// Represents a single bullet
+        /// </summary>
+        private class Bullet
+        {
+            /// <summary>
+            /// The image of the bullet to draw
+            /// </summary>
+            internal Texture2D bullet;
+            /// <summary>
+            /// Where the bullet will be placed.
+            /// </summary>
+            internal Rectangle bulletPosition;
+            /// <summary>
+            /// Where the bullet will be pointing. 
+            /// <see cref="Proyecto.Tanks.AssetsWrappers.Orientation"/>
+            /// </summary>
+            private Orientation currentOrientation;
+            /// <summary>
+            /// The point to use as a reference to rotate the bullet if necessary
+            /// </summary>
+            internal Vector2 rotationPoint;
+            /// <summary>
+            /// The angle to rotate the bullet. Remember that this comes on radians
+            /// instead degrees.
+            /// </summary>
+            internal float rotationAngle;
+            /// <summary>
+            /// How many pixels the bullet will move per iteration
+            /// </summary>
+            private const int SPEED = 1;
+
+            public Bullet()
+            {
+                bulletPosition = new Rectangle() { Width = 15, Height = 10 };
+            }
+
+            /// <summary>
+            /// Defines if the bullet is visible
+            /// </summary>
+            public bool IsBulletVisible { get; private set; }
+
+            /// <summary>
+            /// Moves the bullet by using the direction, speed in this class
+            /// </summary>
+            public void Move(Rectangle windowSize)
+            {
+                //Bullet out of bounds
+                if (bulletPosition.Y < 0 || bulletPosition.X < 0 ||
+                    bulletPosition.Y > windowSize.Height || bulletPosition.X > windowSize.Width)
+                {
+                    IsBulletVisible = false;
+                    return;
+                }
+
+
+                switch (currentOrientation)
+                {
+                    case Orientation.DOWN:
+                        bulletPosition.Y += SPEED;
+                        break;
+                    case Orientation.UP:
+                        bulletPosition.Y -= SPEED;
+                        break;
+                    case Orientation.LEFT:
+                        bulletPosition.X -= SPEED;
+                        break;
+                    case Orientation.RIGHT:
+                        bulletPosition.X += SPEED;
+                        break;
+
+                }
+            }
+
+            /// <summary>
+            /// Starts moving the bullet.
+            /// </summary>
+            /// <param name="theTank">The thank that contains the bullet.</param>
+            public void Start(Tank theTank)
+            {
+                IsBulletVisible = true;
+
+                currentOrientation = theTank.currentOrientation;
+                rotationPoint.X = bullet.Width / 2;
+                rotationPoint.Y = bullet.Height / 2;
+                switch (currentOrientation)
+                {
+                    case Orientation.DOWN:
+                        rotationAngle = MathHelper.PiOver2;
+                        bulletPosition.X = (int)theTank.tankPosition.X + (theTank.spriteToDraw.Width / 2);
+                        bulletPosition.Y = (int)theTank.tankPosition.Y + theTank.spriteToDraw.Height;
+                        break;
+                    case Orientation.UP:
+                        rotationAngle = MathHelper.PiOver2 * -1;
+                        bulletPosition.X = (int)theTank.tankPosition.X + (theTank.spriteToDraw.Height / 2) - 4;
+                        bulletPosition.Y = (int)theTank.tankPosition.Y;
+                        break;
+                    case Orientation.LEFT:
+                        rotationAngle = MathHelper.Pi;//180 Degrees
+                        bulletPosition.X = (int)theTank.tankPosition.X;
+                        bulletPosition.Y = (int)theTank.tankPosition.Y + (theTank.spriteToDraw.Height / 2) - 4;
+                        break;
+                    case Orientation.RIGHT:
+                        rotationAngle = 0;
+                        bulletPosition.X = (int)theTank.tankPosition.X + theTank.spriteToDraw.Width;
+                        bulletPosition.Y = (int)theTank.tankPosition.Y + (theTank.spriteToDraw.Height / 2) - 4;
+                        break;
+
+                }
+            }
+        }
+
+        #region UI_ASSETS
+        /// <summary>
+        /// The name of the asset to load.
+        /// </summary>
+        private string assetName = "tank";
+        private string bulletAssetName = "bullet";
+        private Color tankColor;
+        private Texture2D tank;
+        private Rectangle spriteToDraw;
+        /// <summary>
+        /// The direction of the tank.
+        /// </summary>
+        private Orientation currentOrientation = Orientation.RIGHT;
+        #endregion
+
+        #region MOVEMENT
+        private bool canMove = true;
+        private Bullet myBullet = new Bullet();
+        private Vector2 tankPosition;
+
+        /// <summary>
+        /// Returns the tankPosition of this tank.
+        /// </summary>
+        public IList<Vector2> TankBoundaries { get; private set; }
+        private ushort tankSpeed = 3;//the speed of the tank. 
+        #endregion
+
+        private Game owner;
+        #region INTERACTION_KEYS
+        private Keys DOWN_KEY;
+        private Keys UP_KEY;
+        private Keys LEFT_KEY;
+        private Keys RIGHT_KEY;
+        private Keys FIRE_KEY;
+        #endregion
+
+        public Tank(Game owner)
+        {
+            Random r = new Random(owner.TargetElapsedTime.Milliseconds);
+            TankBoundaries = new List<Vector2>();
+            spriteToDraw = new Rectangle(0, 0, 46, 48);
+            tankPosition = new Vector2(0, 0);
+
+            this.owner = owner;
+            DOWN_KEY = Keys.Down;
+            UP_KEY = Keys.Up;
+            LEFT_KEY = Keys.Left;
+            RIGHT_KEY = Keys.Right;
+            FIRE_KEY = Keys.LeftControl;
+            tankColor = Color.FromNonPremultiplied(r.Next(0, 255), r.Next(0, 255), r.Next(0, 255), 255);
+
+        }
+
+        public override void LoadResources(Microsoft.Xna.Framework.Content.ContentManager content)
+        {
+            tank = content.Load<Texture2D>(assetName);
+            myBullet.bullet = content.Load<Texture2D>(bulletAssetName);
+        }
+
+        public override void Update(Microsoft.Xna.Framework.GameTime gameTime)
+        {
+            var keyStroke = Keyboard.GetState();
+            Keys[] pressedKeys = keyStroke.GetPressedKeys();
+            bool tankMoved = false;
+            if (myBullet.IsBulletVisible) //we gotta move the bullet
+            {
+                myBullet.Move(owner.Window.ClientBounds);
+            }
+            else
+            {
+                //Shoot only one bullet at the time
+                if (pressedKeys.Contains(FIRE_KEY))
+                {
+
+                    myBullet.Start(this);
+                }
+            }
+
+
+            if (canMove)
+            {
+                //the tank can move freely
+
+                if (pressedKeys.Contains(DOWN_KEY))
+                {
+                    if (currentOrientation != Orientation.DOWN)
+                    {
+                        //Down key, rotate it if necessary and move it down in the next frame
+                        spriteToDraw.X = 147;
+                        currentOrientation = Orientation.DOWN;
+                    }
+                    else
+                    {
+                        if (tankPosition.Y < owner.Window.ClientBounds.Height - 53)
+                        {
+                            //Make the tank move down.
+                            tankPosition.Y += tankSpeed;
+                            tankMoved = true;
+                        }
+                    }
+                }
+
+                if (pressedKeys.Contains(UP_KEY))
+                {
+                    if (currentOrientation != Orientation.UP)
+                    {
+                        //Down key, rotate it if necessary and move it down in the next frame
+                        spriteToDraw.X = 96;
+                        currentOrientation = Orientation.UP;
+                    }
+                    else
+                    {
+                        //Make the tank move up.
+                        if (tankPosition.Y > tankSpeed)
+                        {
+                            // avoid the out of bounds.
+                            tankPosition.Y -= tankSpeed;
+                            tankMoved = true;
+                        }
+
+                    }
+                }
+
+                if (pressedKeys.Contains(LEFT_KEY))
+                {
+                    if (currentOrientation != Orientation.LEFT)
+                    {
+                        //Down key, rotate it if necessary and move it down in the next frame
+                        spriteToDraw.X = 48;
+                        currentOrientation = Orientation.LEFT;
+                    }
+                    else
+                    {
+                        //Make the tank move left.
+                        if (tankPosition.X > tankSpeed)
+                        {
+                            tankPosition.X -= tankSpeed;
+                            tankMoved = true;
+                        }
+
+                    }
+                }
+
+                if (pressedKeys.Contains(RIGHT_KEY))
+                {
+                    if (currentOrientation != Orientation.RIGHT)
+                    {
+                        //Down key, rotate it if necessary and move it down in the next frame
+                        spriteToDraw.X = 0;
+                        currentOrientation = Orientation.RIGHT;
+                    }
+                    else
+                    {
+
+                        //Make the tank move right.
+                        if (tankPosition.X < owner.Window.ClientBounds.Width - 53)
+                        {
+                            tankPosition.X += tankSpeed;
+                            tankMoved = true;
+                        }
+                    }
+                }
+                if (tankMoved)
+                {
+                    UpdateTankBoundaries(); 
+                }
+            }
+            
+
+        }
+
+        private void UpdateTankBoundaries()
+        {
+            TankBoundaries.Clear();
+            TankBoundaries.Add(tankPosition);
+            for (int i = (int)tankPosition.X; i < tank.Width; i++)
+            {
+                TankBoundaries.Add(new Vector2(i,1));
+            }
+        }
+
+        public override void Draw(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(tank, tankPosition, spriteToDraw, tankColor);
+
+            if (myBullet.IsBulletVisible)
+            {
+                spriteBatch.Draw(myBullet.bullet, myBullet.bulletPosition, null, tankColor, myBullet.rotationAngle, myBullet.rotationPoint, SpriteEffects.None, 0f);
+            }
+        }
+
+
+        internal void StopMoving(bool stopMoving)
+        {
+            this.canMove = !stopMoving;
+        }
+    }
+}
